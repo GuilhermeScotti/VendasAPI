12bbe3d8411bbe030d9f6899f39c0f4655b7f8ef
Guilherme Scotti <glscotti@hotmail.com>
2024-10-04 07:46:21 -0300

    emitir mensagens

--- Api/Program.cs
+++ Api/Program.cs
@@ -6,12 +6,15 @@ using Microsoft.AspNetCore.Diagnostics;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddScoped<IVendaRepository, VendaRepository>();
 builder.Services.AddScoped<IRepository<Venda>, VendaRepository>();
 builder.Services.AddScoped<IVendaProdutoRepository, VendaProdutoRepository>();
 builder.Services.AddScoped<IRepository<VendaProduto>, VendaProdutoRepository>();
 builder.Services.AddScoped<IRepository<Produto>, ProdutoRepository>();
 builder.Services.AddScoped<INumeroVendaRepository, NumeroVendaRepository>();
+
+builder.Services.AddSingleton<IServiçoDeMensageria, ServiçoDeMensageria>();
 
 var app = builder.Build();
 
@@ -23,7 +26,9 @@ app.Map("/error", (HttpContext httpContext) =>
 
   // Logar exception
 
-  return Results.Problem("Houve um erro na operação.", statusCode: 500);
+  var mensagem = exception?.Message ?? "desconhecido";
+
+  return Results.Problem($"Houve um erro na operação: {mensagem}", statusCode: 500);
 });
 
 app.MapGet("/", () => "Bem-vindo à API de vendas!");
@@ -49,44 +54,67 @@ app.MapGet("/vendas/{id}/detalhes", async (Guid id, IVendaRepository repo) =>
 })
 .WithName("ObterVendaCompletaPorId");
 
-app.MapPost("/vendas", async (CriarVendaDto novaVenda, IVendaRepository repo) =>
+app.MapPost("/vendas", async (CriarVendaDto novaVenda, IVendaRepository repo, IServiçoDeMensageria mensageria) =>
 {
   var vendaCriada = await repo.AdicionarDeDtoAsync(novaVenda);
+
+  await mensageria.CompraCriada();
   return Results.Created($"/vendas/{vendaCriada.Id}", vendaCriada);
 })
 .WithName("CriarVenda");
 
-app.MapPatch("/vendas/{id}/cancelar", async (Guid id, CancelarVendaDto atualizarVendaDto, IRepository<Venda> repo) =>
+app.MapPatch("/vendas/{id}/cancelar", async (Guid id, CancelarVendaDto atualizarVendaDto, IRepository<Venda> repo, IServiçoDeMensageria mensageria) =>
 {
   var venda = await repo.ObterPorIdAsync(id);
 
-  if (venda is null) return Results.NotFound();
+  if (venda is null)
+    return Results.NotFound();
+
+  if (venda.Cancelado)
+    return Results.Problem("Venda já cancelada.");
 
   await repo.AtualizarAsync(venda with
   {
     Cancelado = true,
     MotivoCancelamento = atualizarVendaDto.MotivoCancelamento
   });
+
+  await mensageria.CompraCancelada();
   return Results.NoContent();
 })
 .WithName("CancelarVenda");
 
 app.MapPost("/vendas/{id}/produtos", async (Guid id, VenderProdutosDto venderProdutosDto,
   IVendaProdutoRepository repo,
-  IRepository<Venda> vendaRepo) =>
+  IRepository<Venda> vendaRepo,
+  IServiçoDeMensageria mensageria) =>
 {
   var venda = await vendaRepo.ObterPorIdAsync(id);
 
-  if (venda is null) return Results.NotFound();
+  if (venda is null)
+    return Results.NotFound();
+
+  if (venda.Cancelado)
+    return Results.Problem("Venda cancelada. Não é possível adicionar produtos.");
 
   await repo.VenderProdutosAsync(id, venderProdutosDto);
+
+  await mensageria.CompraAlterada();
   return Results.NoContent();
 })
 .WithName("AdicionarProdutosVenda");
 
-app.MapDelete("/vendas/{id}", async (Guid id, IRepository<Venda> repo) =>
+
+
+app.MapDelete("/vendas/{id}", async (Guid id, IRepository<Venda> repo, IServiçoDeMensageria mensageria) =>
 {
   var deleted = await repo.DeletarAsync(id);
+
+  if (deleted)
+    await mensageria.CompraExcluida();
+  else
+    return Results.Problem("Não foi possível excluir venda.");
+
   return deleted ? Results.NoContent() : Results.NotFound();
 })
 .WithName("DeletarVenda");
